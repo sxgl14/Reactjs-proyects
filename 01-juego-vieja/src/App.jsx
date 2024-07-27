25eac3eadd7bc375040985f15a2bbf58c92ec5a8
Yorgel_P <yorgelpaez14@gmail.com>
2024-07-26 23:25:17 -0400

    added the change of turn and cannot be updated after placing an element

--- 01-juego-vieja/src/App.jsx
+++ 01-juego-vieja/src/App.jsx
@@ -5,34 +5,59 @@ const TURNS = {
   O: 'o'
 }
 
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {    //
+    updateBoard(index)
+  }
+
   return (
-    <div className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
 
 function App() {
-  const [board, setBoard] = useState(['x', 'x', 'x', 'o', 'o', 'o', 'x', 'o', 'x'])
-  console.log(board)
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+  )
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = (index) => {
+    if (board[index]) return    //If the position already has an item, it is not updated
+    const newBoard = [...board]  //Update the board for that appear the X or O
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ?  TURNS.O : TURNS.X //If the turn already was of X, the next turn be for O and vice versa
+    setTurn(newTurn)
+  }
 
   return (
     <main className='board'>
-      <h1>3 EN LINEA</h1>
+      <h1>THREE IN LINE</h1>
       <section className='game'>
         {
-          board.map((cell, index) => {
+          board.map((cell, index) => {  //Board where elements X and O appear
             return(
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
               >
                 {board[index]}
               </Square>
             )
           })
         }
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>  {/* Desing where it shows the turn */}
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
     )
